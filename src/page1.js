997f7e334589c97854953618414a87e62b2afdb5
PERSEPHONE\user <isabella.hammann@study.hs-duesseldorf.de>
2025-06-09 20:39:32 +0200

    Lederbleumchen Beschreibung als Image hinzugefügt

--- src/page1.js
+++ src/page1.js
@@ -44,20 +44,20 @@ AFRAME.registerComponent("gltf-with-animation", {
             }
 
             // Titel anzeigen
-            const nameText = document.createElement("a-text");
-            nameText.setAttribute("value", plant.name);
-            nameText.setAttribute("position", "0.8 0.8 0");
-            nameText.setAttribute("color", "#59772f");
-            nameText.setAttribute("width", 2);
-            nameText.setAttribute("align", "center");
-            el.appendChild(nameText);
+            // const nameText = document.createElement("a-text");
+            // nameText.setAttribute("value", plant.name);
+            // nameText.setAttribute("position", "0.8 0.8 0");
+            // nameText.setAttribute("color", "#59772f");
+            // nameText.setAttribute("width", 2);
+            // nameText.setAttribute("align", "center");
+            // el.appendChild(nameText);
 
             // Beschreibung als Bild anzeigen
             const descImage = document.createElement("a-image");
             descImage.setAttribute("src", plant.descriptionImage); // PNG aus dem JSON
-            descImage.setAttribute("position", "0.8 0.6 0");
-            descImage.setAttribute("width", 1.5); // ggf. anpassen
-            descImage.setAttribute("height", 0.5);
+            descImage.setAttribute("position", "0.8 1 0");
+            descImage.setAttribute("width", 1);
+            descImage.setAttribute("height", 1);
             el.appendChild(descImage);
 
             // Bild anzeigen
